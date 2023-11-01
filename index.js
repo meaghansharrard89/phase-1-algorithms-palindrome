6368523a5d3c148e5331d11db7db77138fb5bb0f
Meaghan <meaghan.sharrard89@gmail.com>
2023-11-01 16:33:14 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(string) {
+  //get the length of the string and store it in a var "result"
+  let result = string.length
+  //create a for loop to divide the length in half
+  for (let i = 0; i < result / 2; i++) {
+    //check if the first and last string are the same
+    if (string[i] !== string[result - 1 - i]) {
+      //if they don't match, return false
+    return false;
+} else {
+  //if they do match, return true
+  return true;
 }
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+  }
+}
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
